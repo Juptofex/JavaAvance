64c9b83edb6ad97d46e65e9516d71ccb6f81e625
juptofex <julien.damilot@gmail.com>
2024-10-24 08:03:34 +0200

    Seance1-2 Semaine6 INIT

---
+++ AJ_atelier05/tests/usecase/ListeProduitsTest.java
@@ -0,0 +1,43 @@
+package usecase;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListeProduitsTest {
+
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void contient() {
+    }
+
+    @Test
+    void ajouterProduit() {
+    }
+
+    @Test
+    void supprimerProduit() {
+    }
+
+    @Test
+    void trouverProduit() {
+    }
+
+    @Test
+    void produits() {
+    }
+
+    @Test
+    void ajouterPrix() {
+    }
+
+    @Test
+    void trouverPrix() {
+    }
+}
